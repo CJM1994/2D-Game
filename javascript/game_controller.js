b4e6c24c0cc0ba979550beebb084b112f7e08e83
CJM1994 <connorjohnmullin@gmail.com>
2022-06-12 23:41:59 -0700

    add movement to player

--- javascript/game_controller.js
+++ javascript/game_controller.js
@@ -1,6 +1,11 @@
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
+const KEY_LEFT = 37;
+const KEY_DOWN = 38;
+const KEY_RIGHT = 39;
+const KEY_UP = 40;
+
 const gameState = {
     player: {
         x: 0,
@@ -29,4 +34,29 @@ const createPlayer = ($container) => {
     setPosition($player, gameState.player.x, gameState.player.y);
 }
 
+const onKeyDown = (event) => {
+    const $player = document.querySelector('.player');
+    console.log(event.keyCode);
+
+    switch (event.keyCode) {
+        case KEY_LEFT:
+            gameState.player.x -= 5;
+            setPosition($player, gameState.player.x, gameState.player.y);
+            break;
+        case KEY_UP:
+            gameState.player.y += 5;
+            setPosition($player, gameState.player.x, gameState.player.y);
+            break;
+        case KEY_RIGHT:
+            gameState.player.x += 5;
+            setPosition($player, gameState.player.x, gameState.player.y);
+            break;
+        case KEY_DOWN:
+            gameState.player.y -= 5;
+            setPosition($player, gameState.player.x, gameState.player.y);
+            break;
+    }
+}
+
 init();
+document.addEventListener('keydown', onKeyDown);
